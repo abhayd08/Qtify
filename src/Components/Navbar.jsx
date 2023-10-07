a07f88a8e8fc1e7f5c1f2533a6dd5d462cd95acc
Abhay Dixit <abhaydt.08@gmail.com>
2023-10-07 17:51:21 +0530

    C4: Search Bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,15 @@
 import styles from "./Navbar.module.css";
 import Button from "./Button";
 import logo from "../assets/logo.svg";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => {
   return (
-    <>
-      <nav className={styles.navbar}>
-        <img src={logo} alt="Qtify" />
-        <Button children="Give Feedback" />
-      </nav>
-    </>
+    <nav className={styles.navbar}>
+      <img src={logo} alt="Qtify" />
+      <SearchBar className={styles.navbar} />
+      <Button children="Give Feedback" />
+    </nav>
   );
 };
 
